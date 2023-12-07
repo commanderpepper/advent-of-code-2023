b0576f3ed214ab7ded9c1a21af135fa48938c1ea
commanderpepper <ahmadhumza7@gmail.com>
2023-12-06 21:59:54 -0800

    Solved day two part two

--- src/days/Day02.kt
+++ src/days/Day02.kt
@@ -1,6 +1,5 @@
 package days
 
-import println
 import readInput
 
 fun main(){
@@ -10,6 +9,7 @@ fun main(){
     println(dayTwoInput)
 
     println(sumOfValidGameIdsPartOne(dayOneInput))
+    println(sumOfPowerOfASet(dayTwoInput))
 }
 
 private fun sumOfValidGameIdsPartOne(dayOneInput: List<String>): Int {
@@ -63,7 +63,60 @@ private fun sumOfValidGameIdsPartOne(dayOneInput: List<String>): Int {
     return dayOneAmount
 }
 
-data class CubeGame(val redCubes: Int = 0, val blueCubes: Int = 0, val greenCubes: Int = 0)
+private fun sumOfPowerOfASet(dayTwoInput: List<String>): Int {
+    val dayTwoGameMap = mutableMapOf<Int, CubeGame>()
+
+    val splitGames = dayTwoInput.map { it.split(":") }
+    splitGames.forEach {
+        val key = it.first()
+        val sets = it.last().split(";")
+
+        val numberedKey = key.split(" ").last().toInt()
+        dayTwoGameMap[numberedKey] = CubeGame()
+
+        sets.forEach { set ->
+            val cubes = set.split(",")
+            cubes.forEach { cube ->
+                val amountAndColor = cube.trim().split(" ")
+                val amount = amountAndColor.first().toInt()
+                val color = amountAndColor.last()
+                val cubeGame = dayTwoGameMap[numberedKey]
+                when (color) {
+                    "blue" -> {
+                        if (amount > cubeGame!!.blueCubes) {
+                            dayTwoGameMap[numberedKey] = cubeGame!!.copy(blueCubes = amount)
+                        }
+                    }
+
+                    "red" -> {
+                        if (amount > cubeGame!!.redCubes) {
+                            dayTwoGameMap[numberedKey] = cubeGame!!.copy(redCubes = amount)
+                        }
+                    }
+
+                    "green" -> {
+                        if (amount > cubeGame!!.greenCubes) {
+                            dayTwoGameMap[numberedKey] = cubeGame!!.copy(greenCubes = amount)
+                        }
+                    }
+
+                    else -> {
+
+                    }
+                }
+            }
+        }
+    }
+    var sumOfPowers = 0
+    dayTwoGameMap.forEach { (id, game) ->
+        sumOfPowers += game.getPower()
+    }
+    return sumOfPowers
+}
+
+data class CubeGame(val redCubes: Int = 0, val blueCubes: Int = 0, val greenCubes: Int = 0){
+    fun getPower(): Int = redCubes * blueCubes * greenCubes
+}
 
 fun CubeGame.isGamePossible(possibleRedCubes: Int, possibleBlueCubes: Int, possibleGreenCubes: Int): Boolean {
     return this.redCubes <= possibleRedCubes && this.blueCubes <= possibleBlueCubes && this.greenCubes <= possibleGreenCubes
