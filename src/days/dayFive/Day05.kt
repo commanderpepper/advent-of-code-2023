f30327007fcf2b7538d03b9e2a411aaa0b62c304
commanderpepper <ahmadhumza7@gmail.com>
2023-12-16 19:41:53 -0800

    Set up for day five

---
+++ src/days/dayFive/Day05.kt
@@ -0,0 +1,10 @@
+package days.dayFive
+
+import helpers.readInput
+
+fun main(){
+    val dayOneInput = readInput("Day05PartOne")
+    val dayTwoInput = readInput("Day05PartTwo")
+    println(dayOneInput)
+    println(dayTwoInput)
+}
