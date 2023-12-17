5da3cb4044b1c26d829d8f181158abe72b9892e7
commanderpepper <ahmadhumza7@gmail.com>
2023-12-16 20:53:53 -0800

    Parsed the input.

--- src/days/dayFive/Day05.kt
+++ src/days/dayFive/Day05.kt
@@ -7,4 +7,57 @@ fun main(){
     val dayTwoInput = readInput("Day05PartTwo")
     println(dayOneInput)
     println(dayTwoInput)
+
+    val seeds = getSeeds(dayOneInput.first())
+    println(seeds)
+
+    val seedToSoil = getAlmanacMap(dayOneInput, "seed-to-soil map:")
+    seedToSoil.map.forEach(::println)
+
+    println()
+
+    val soilToFertilizer = getAlmanacMap(dayOneInput, "soil-to-fertilizer map:")
+    val fertilizerToWater = getAlmanacMap(dayOneInput, "fertilizer-to-water map:")
+    val waterToLight = getAlmanacMap(dayOneInput, "water-to-light map:")
+    val lightToTemperature = getAlmanacMap(dayOneInput, "light-to-temperature map:")
+    val temperatureToHumidity = getAlmanacMap(dayOneInput, "temperature-to-humidity map:")
+    val humidityToLocation = getAlmanacMap(dayOneInput, "humidity-to-location map:")
+
+
 }
+
+fun getSeeds(seedsAsString: String): List<Seed>{
+    val seeds = seedsAsString.split(":")
+        .last()
+        .split(" ")
+        .map { it.trim() }
+        .filter { it.isNotBlank() }
+        .map { Seed(it.toInt()) }
+    return seeds
+}
+
+fun getAlmanacMap(data: List<String>, target: String): AlmanacMap{
+    val index = data.indexOfFirst { it.contains(target) }
+    val map = mutableListOf<AlmanacCategory>()
+    if(index != -1){
+        var i = index + 1
+        while(data[i].any { it.isDigit() }){
+            val mapLine = data[i]
+            val numbers = mapLine.split(" ")
+            val source = numbers[1]
+            val destination = numbers[0]
+            val range = numbers[2]
+            for(i in 0 until range.toInt()){
+                map.add(AlmanacCategory(source = source.toInt() + i, destination = destination.toInt() + i))
+            }
+            i++
+        }
+    }
+    return AlmanacMap(map)
+}
+
+data class Seed(val seedType: Int)
+
+data class AlmanacMap(val map: List<AlmanacCategory>)
+
+data class AlmanacCategory(val source: Int, val destination: Int)
