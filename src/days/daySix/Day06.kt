2cadc48b6ce5b1e9f594cd09ffaf920609e440c2
commanderpepper <ahmadhumza7@gmail.com>
2023-12-22 21:47:37 -0800

    Added set up for day six

---
+++ src/days/daySix/Day06.kt
@@ -0,0 +1,10 @@
+package days.daySix
+
+import helpers.readInput
+
+fun main(){
+    val dayOneInput = readInput("Day06PartOne")
+    val dayTwoInput = readInput("Day06PartTwo")
+    println(dayOneInput)
+    println(dayTwoInput)
+}
